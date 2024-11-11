8d53ed9a1f71dc02cc00e4561d12d0c636b402da
guptaharshit567 <guptaharshit567@gmail.com>
2024-11-11 19:46:00 +0530

    dre

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left"></div>
+          <div className="right">
+            <p>Kandivali West, Mumbai</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="right">
+          <p>All Right Reserved | Designed By Aditya Singh</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
